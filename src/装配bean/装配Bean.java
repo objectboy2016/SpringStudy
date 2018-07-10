b1f0d077fc7140eaba0f04393cd2a059e617bc82
objectboy <objectboy@msn.com>
2018-07-10 16:49:11 +0800

    ll

---
+++ src/装配bean/装配Bean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package 装配bean;
+
+import org.springframework.context.ApplicationContext; 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Administrator
+ */
+public class 装配Bean {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("services.xml");   
+        SgtPeppers compactDisc = context.getBean(SgtPeppers.class);
+        compactDisc.play();
+    }
+}
